cebbad1106f96cb55c5cedddccfe3f8c92cdff22
MengtMa <mengtongma@gmail.com>
2018-11-12 14:09:58 -0800

    limit selected strategy amount

--- server.py
+++ server.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def getInput():
     return render_template('home.html')
+
+@app.route("/result", methods=['GET', 'POST'])
+def getResult():
+    if request.method == 'POST':
+        amount = request.form.get('amount', None)
+        strategy = request.form.getlist("strategy")
+        return render_template('result.html', amount=amount, strategy=strategy)
 
 if __name__ == '__main__':
     app.run(debug = True)
